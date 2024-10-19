c8a446e9328d238ef137ae68c195a28f522bc622
Aliaksandr Batsin <aliaksandr_batsin@epam.com>
2024-10-19 22:13:00 +0400

    add language button

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
+# Python imports
 from flask import Flask, render_template, request, jsonify, send_file
 import requests
 import azure.cognitiveservices.speech as speechsdk
 import tempfile
 import os
 
+# Create the Flask app
 app = Flask(__name__)
 
+# Serve the index.html page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Handle image uploads and OCR processing
 @app.route('/upload-image', methods=['POST'])
 def upload_image():
     if 'image' not in request.files:
@@ -23,6 +27,7 @@ def upload_image():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+# Process image to extract text through the OCR API
 def process_image(file):
     api_endpoint = "https://eastus.api.cognitive.microsoft.com/vision/v3.2/ocr"
     api_key = os.environ.get('OCR_API_KEY')
@@ -46,14 +51,25 @@ def process_image(file):
     text_output = ' '.join([' '.join([word['text'] for word in line['words']]) for region in analysis.get('regions', []) for line in region['lines']])
     return text_output
 
+# Synthesize text into speech using Azure AI
 @app.route('/synthesize-speech', methods=['POST'])
 def synthesize_speech():
-    text = request.data.decode('utf-8')
+    data = request.get_json()
+    text = data.get('text')
+    language = data.get('language', 'en')
+
     if text:
         speech_key = os.environ.get('AZURE_SPEECH_KEY')
         service_region = os.environ.get('AZURE_SERVICE_REGION')
         speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
-        speech_config.speech_synthesis_voice_name = "uk-UA-OstapNeural"
+
+        # Set the voice based on the chosen language
+        if language == 'uk':
+            speech_config.speech_synthesis_voice_name = "uk-UA-OstapNeural"
+        elif language == 'ru':
+            speech_config.speech_synthesis_voice_name = "ru-RU-DmitryNeural"
+        else:  # Default to English
+            speech_config.speech_synthesis_voice_name = "en-US-JennyNeural"
 
         synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=None)
         result = synthesizer.speak_text_async(text).get()
@@ -72,6 +88,7 @@ def synthesize_speech():
 
     return jsonify({'error': 'No text provided'}), 400
 
+# Run the application using Waitress if executed directly
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8000)
